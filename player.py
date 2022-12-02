1f01d86b63fbf10d790c1e10a9e1f55c0b269448
strculande <carree.toitoine@gmail.com>
2022-12-02 11:02:43 +0100

    Update player.py

--- player.py
+++ player.py
@@ -3,28 +3,31 @@ from abc import ABC, abstractmethod
 
 class Player(ABC):
     
-    def __init__(self,name,score):
-        self.score = score
+    def __init__(self,name):
+        self.score = 0
         self.name = name
         self.__main = []
         self.statu = []
-        self._carte_max = 5
-        
+        self.__carte_max = 5
+    
+    def __str__ (self):
+        Aff = "-"*20 
+        return Aff+f"\n name = {self.name}"+f"\n score = {self.score}"+f"\n card = {self.__main}"+'\n'+Aff
        
     @property    
-    def tirer_carte(self):
+    def pick_card(self):
         return self.__main
     
-    @tirer_carte.setter
-    def tirer_carte(self,carte):
-        self.__main.append(carte) if len(self.__main) <= self.__carte_max else print("deffauser, car trop de cartes")        
+    @pick_card.setter
+    def pick_card(self,carte):
+        self.__main.append(carte) if len(self.__main) < self.__carte_max else print("deffauser, car trop de cartes")        
         
     @property
     def score(self):
         return self.__score
     @score.setter
     def score(self,score):
-        if score > 0:
+        if score >= 0:
             self.__score = score
         else:
             print("impssible de retirer des point au joueur")
@@ -39,29 +42,27 @@ class IA(Player):
 
 
 
-class Humain(Player):
+class Human(Player):
     
-    def __init__(self,name,score):
-        super().__init__(name,score)
+    def __init__(self,name):
+        super().__init__(name)
         self.__main = []
 
-    def poser_carte(self):
+    def play_card(self):
             return input("ou voulez vous poser votre carte ?")
     
-    def selec_choice(self):
+    def sel_choice(self):
             return input("quelle action voulez vous faire ?")
        
-    def supp_carte (self,place):
-        try:
-            del self.__main[place-1]
-            
-        except:
-            if place != int :
-               return print("erreur emplacement de carte invalide")
+    def del_card (self,card):
+        
+            if card in self.main:
+                self.__main.remove(card)
             else: 
-               return print("erreur lors de la supression de carte")
+               return print("cette carte n est pas presente dans votre main")
 
 """
 le score doit etre une input qui ne fonctione pas sur spider
 
 """
+P1 = Human("antoine")
